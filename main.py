8066445db5e17b139a22ff3ccbcbcd455d1b7693
matthewshay2r <mdshaiturov@edu.hse.ru>
2024-06-03 02:49:38 +0300

    v0.1

---
+++ main.py
@@ -0,0 +1,38 @@
+from utils.core import create_sessions
+from utils.telegram import Accounts
+from utils.blum import Blum
+from data.config import hello, USE_PROXY
+import asyncio
+
+
+async def main():
+    print(hello)
+    action = int(input('Выберите действие:\n1. Начать фарм\n2. Создать сессию\n>'))
+
+    if action == 2:
+        await create_sessions()
+
+    if action == 1:
+        accounts = await Accounts().get_accounts()
+        proxy_dict = {}
+        with open('proxy.txt', 'r') as file:
+            proxy = [i.strip().split() for i in file.readlines() if len(i.strip().split()) == 2]
+            for prox, name in proxy:
+                proxy_dict[name] = prox
+
+        tasks = []
+        if USE_PROXY:
+            for thread, account in enumerate(accounts):
+                if account in proxy_dict:
+                    tasks.append(
+                        asyncio.create_task(Blum(account=account, thread=thread, proxy=proxy_dict[account]).main()))
+                else:
+                    tasks.append(asyncio.create_task(Blum(account=account, thread=thread, proxy=None).main()))
+        else:
+            for thread, account in enumerate(accounts):
+                tasks.append(asyncio.create_task(Blum(account=account, thread=thread, proxy=None).main()))
+        await asyncio.gather(*tasks)
+
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
